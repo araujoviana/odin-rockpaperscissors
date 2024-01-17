5fb676219ee8ad688550f38aec099e7a9197d726
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-01-17 16:45:14 -0300

    Add script pseudocode

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
-console.log("HEY");
+
+function getComputerChoice() {
+    // Returns randomly rock, paper or scissors
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    // Compare the two values
+    // Check who wins
+    // Print message like "You Lose! Paper beats Rock"
+    // Ties replay the round
+  }
+   
+  const playerSelection = "rock"; // use prompt
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+
+  function game() {
+  // Does a best of five games
+  // Keeps the score
+  // Print the result of each game
+  // Prints if you win or lose at the end
+  }
